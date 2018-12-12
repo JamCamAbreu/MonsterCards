972fc039ce0e8b04d577cc0c1f5e75304a8ec78c
James Cameron Abreu <coolcam222@gmail.com>
2018-12-12 11:45:34 -0700

    a few early ideas

--- MonsterCards/MonsterCards/Program.cs
+++ MonsterCards/MonsterCards/Program.cs
@@ -5,8 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MonsterCards {
+
+
+  // Abstract class:
+  public class card {
+    int _maxHP;
+    int HP;
+    int defence;
+    bool active; // whether or not can attack
+  }
+
+
+
+
+
+
   class Program {
+
+
+    // ENTRY POINT FOR PROGRAM:
     static void Main(string[] args) {
+
+
+
+
+      // End of program
+      Console.WriteLine("Press any key to exit");
+      Console.ReadKey();
     }
+
+
   }
 }
